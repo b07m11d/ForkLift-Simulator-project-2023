3ee53b8069d13e25c495d4e95f95228329ed0faa
b07m11d <38603632+b07m11d@users.noreply.github.com>
2023-12-08 15:19:13 +0800

    2023-12-08 15:17

---
+++ Forklift_Simulate_2023/IfGoodsOnShelf.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfGoodsOnShelf : MonoBehaviour
+{
+    //貨架上專用
+    public static bool isGoodsOnMe_sf = false;
+    public bool isGoodsOnMe_ins = false;
+    public static bool ThisisGoodsShelf = false;
+
+    public string ColliderName;
+    int count = 0;
+
+    void Start()
+    {
+        Init();
+    }
+
+    public void Init()
+    {
+        isGoodsOnMe_sf = false;
+        isGoodsOnMe_ins = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Goods")
+        {
+            isGoodsOnMe_sf = true;
+            isGoodsOnMe_ins = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Goods")
+        {
+            isGoodsOnMe_sf = false;
+            isGoodsOnMe_ins = false;
+        }
+    }
+}
+
