c549e1f01aef70dec9aef275efc3e7ea7fe837a9
b07m11d <38603632+b07m11d@users.noreply.github.com>
2024-01-30 17:31:59 +0800

    2024-01-30 collison test
    Collison test not yet finished

---
+++ Forklift_Simulate_2023/Assets/CollisonTEST.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisonTEST : MonoBehaviour
+{
+
+    GameObject Good1;
+    
+    void Start()
+    {
+        Good1 = GameObject.Find("Good1"); //找到Good1
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Hi, 123");
+        Rigidbody Good1rb = Good1.GetComponent<Rigidbody>();
+        Good1rb.isKinematic = true;
+        Good1rb.detectCollisions = false;
+    }
+    }
